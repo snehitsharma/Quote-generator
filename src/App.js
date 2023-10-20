4c5836fbf0cbaa96e38dbe548edef4c19c4a027b
snehit sharma <snehitsharma@outlook.com>
2023-10-20 02:12:11 -0400

    sec commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState({ text: '', author: '' }); // Initialize as an empty object
+
+  const getquotes = () => {
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      .then((data) => {
+        let randomnum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomnum]);
+      });
+  };
+
+  useEffect(() => {
+    getquotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='quote'>
+        <p>{quotes.text}</p>
+        <p>{quotes.author}</p>
+      </div>
+      <div className='btn-container'>
+        <button className='btn' onClick={getquotes}>
+          Generate a Quote!
+        </button>
+      </div>
     </div>
   );
 }
